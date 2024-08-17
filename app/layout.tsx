d6d2e678c22b8e130531700f272f9eac282ad47b
Fawwaz Firdaus <fawwazfirdaus7@gmail.com>
2024-08-16 21:12:09 -0700

    Update metadata in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,17 +8,17 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 import { SandpackCSS } from './blog/[slug]/sandpack';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://leerob.io'),
+  metadataBase: new URL('https://fawwazfirdaus.com'),
   title: {
-    default: 'Lee Robinson',
-    template: '%s | Lee Robinson',
+    default: 'Fawwaz Firdaus',
+    template: '%s | Fawwaz Firdaus',
   },
   description: 'Developer, writer, and creator.',
   openGraph: {
-    title: 'Lee Robinson',
+    title: 'Fawwaz Firdaus',
     description: 'Developer, writer, and creator.',
-    url: 'https://leerob.io',
-    siteName: 'Lee Robinson',
+    url: 'https://fawwazfirdaus.com',
+    siteName: 'Fawwaz Firdaus',
     locale: 'en_US',
     type: 'website',
   },
@@ -34,7 +34,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: 'Lee Robinson',
+    title: 'Fawwaz Firdaus',
     card: 'summary_large_image',
   },
   verification: {
