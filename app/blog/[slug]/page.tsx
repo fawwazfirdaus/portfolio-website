5c04b551b89d86fe5a9ebad41b5d6c396e1fc785
Fawwaz Firdaus <fawwazfirdaus7@gmail.com>
2024-08-18 13:19:31 -0700

    Remove unused code in page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -6,7 +6,6 @@ import { getViewsCount } from 'app/db/queries';
 import { getBlogPosts } from 'app/db/blog';
 import ViewCounter from '../view-counter';
 import { increment } from 'app/db/actions';
-import { unstable_noStore as noStore } from 'next/cache';
 import ErrorBoundary from 'app/components/error-boundary';
 import { formatDate } from 'app/utils/formatDate';
 
@@ -51,38 +50,6 @@ export async function generateMetadata({
     },
   };
 }
-
-// function formatDate(date: string) {
-//   noStore();
-//   let currentDate = new Date().getTime();
-//   if (!date.includes('T')) {
-//     date = `${date}T00:00:00`;
-//   }
-//   let targetDate = new Date(date).getTime();
-//   let timeDifference = Math.abs(currentDate - targetDate);
-//   let daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-  
-//   let fullDate = new Date(date).toLocaleString('en-us', {
-//     month: 'long',
-//     day: 'numeric',
-//     year: 'numeric',
-//   });
-
-//   if (daysAgo < 1) {
-//     return 'Today';
-//   } else if (daysAgo < 7) {
-//     return `${fullDate} (${daysAgo}d ago)`;
-//   } else if (daysAgo < 30) {
-//     const weeksAgo = Math.floor(daysAgo / 7)
-//     return `${fullDate} (${weeksAgo}w ago)`;
-//   } else if (daysAgo < 365) {
-//     const monthsAgo = Math.floor(daysAgo / 30)
-//     return `${fullDate} (${monthsAgo}mo ago)`;
-//   } else {
-//     const yearsAgo = Math.floor(daysAgo / 365)
-//     return `${fullDate} (${yearsAgo}y ago)`;
-//   }
-// }
 
 export default async function Blog({ params }) {
   let post = getBlogPosts().find((post) => post.slug === params.slug);
