fbea43098eebe6c6cb2de0f9e580684781a02450
Fawwaz Firdaus <fawwazfirdaus7@gmail.com>
2024-08-18 13:42:42 -0700

    Remove YouTube subscription queries

--- app/db/queries.tsx
+++ app/db/queries.tsx
@@ -15,11 +15,6 @@ let googleAuth = new auth.GoogleAuth({
   scopes: ['https://www.googleapis.com/auth/youtube.readonly'],
 });
 
-let yt = youtube({
-  version: 'v3',
-  auth: googleAuth,
-});
-
 export async function getBlogViews() {
   if (!process.env.POSTGRES_URL) {
     return [];
@@ -47,38 +42,6 @@ export async function getViewsCount(): Promise<
     FROM views
   `;
 }
-
-export const getLeeYouTubeSubs = cache(
-  async () => {
-    let response = await yt.channels.list({
-      id: ['UCZMli3czZnd1uoc1ShTouQw'],
-      part: ['statistics'],
-    });
-
-    let channel = response.data.items![0];
-    return Number(channel?.statistics?.subscriberCount).toLocaleString();
-  },
-  ['fawwaz-youtube-subs'],
-  {
-    revalidate: 3600,
-  }
-);
-
-export const getVercelYouTubeSubs = cache(
-  async () => {
-    let response = await yt.channels.list({
-      id: ['UCLq8gNoee7oXM7MvTdjyQvA'],
-      part: ['statistics'],
-    });
-
-    let channel = response.data.items![0];
-    return Number(channel?.statistics?.subscriberCount).toLocaleString();
-  },
-  ['vercel-youtube-subs'],
-  {
-    revalidate: 3600,
-  }
-);
 
 export async function getGuestbookEntries() {
   if (!process.env.POSTGRES_URL) {
