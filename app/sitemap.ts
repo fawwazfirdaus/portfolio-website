cacd49abd22e2371b637c91d975ca916de8d2635
Fawwaz Firdaus <fawwazfirdaus7@gmail.com>
2024-08-17 20:22:49 -0700

    Update sitemap routes

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,7 +6,7 @@ export default async function sitemap() {
     lastModified: post.metadata.publishedAt,
   }));
 
-  let routes = ['', '/blog', '/guestbook', '/uses', '/work'].map((route) => ({
+  let routes = ['', '/blog', '/guestbook', '/work'].map((route) => ({
     url: `https://fawwazfirdaus.com${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }));
