3649914ebcbb602b29080411bcd97926477252dd
Fawwaz Firdaus <fawwazfirdaus7@gmail.com>
2024-08-19 03:21:57 -0700

    Update buttons.tsx and apple-logo.svg

--- app/guestbook/buttons.tsx
+++ app/guestbook/buttons.tsx
@@ -15,7 +15,7 @@ export function SignOut() {
 
 export function SignIn() {
   return (
-    <div className="flex flex-col space-y-4">
+    <div className="flex flex-col space-x-4">
       <button
         className="px-3 py-2 border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100"
         onClick={() => signIn('github')}
