7cc86d7cf0693ba54bcdb64bfe1dd120fc953c7e
Fawwaz Firdaus <fawwazfirdaus7@gmail.com>
2024-08-19 05:14:19 -0700

    Update guestbook buttons

--- app/guestbook/buttons.tsx
+++ app/guestbook/buttons.tsx
@@ -15,20 +15,20 @@ export function SignOut() {
 
 export function SignIn() {
   return (
-    <div className="flex flex-col space-y-4 mb-8">
+    <div className="flex flex-col space-y-2 mb-8 max-w-[250px]">
       <button
-        className="px-3 py-2 border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100"
+        className="px-3 py-2 border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm flex items-center leading-4 text-neutral-900 dark:text-neutral-100"
         onClick={() => signIn('github')}
       >
-        <img alt="GitHub logo" src="/github-logo.svg" width="20" height="20" />
-        <div className="ml-3">Sign in with GitHub</div>
+        <img alt="GitHub logo" src="/github-logo.svg" width="20" height="20" className="invert dark:invert-0" />
+        <div className="ml-3 flex-grow">Sign in with GitHub</div>
       </button>
       <button
-        className="px-3 py-2 border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100"
+        className="px-3 py-2 border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm flex items-center leading-4 text-neutral-900 dark:text-neutral-100"
         onClick={() => signIn('apple')}
       >
-        <img alt="Apple logo" src="/apple-logo.svg" width="24" height="24" />
-        <div className="ml-3">Sign in with Apple</div>
+        <img alt="Apple logo" src="/apple-logo.svg" width="20" height="20" className="invert dark:invert-0" />
+        <div className="ml-3 flex-grow">Sign in with Apple</div>
       </button>
     </div>
   );
